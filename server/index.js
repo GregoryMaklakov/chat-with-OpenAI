59205d22d13269473a46902041ac1f9b6a53efa5
Gregory <maklakov.grigory@gmail.com>
2023-05-27 12:12:17 +0200

    create & add AI-Bot for chat

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import { Configuration, OpenAIApi } from "openai";
+import openaiRoutes from "./routes/openai.js";
 
 /* Configurations */
 dotenv.config();
@@ -15,6 +17,15 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+/*  Routes */
+app.use("/openai", openaiRoutes);
+
+/* OpenAI Configuration */
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_API_KEY,
+});
+export const openai = new OpenAIApi(configuration);
 
 /* Server Setup */
 const PORT = process.env.PORT || 9000;
