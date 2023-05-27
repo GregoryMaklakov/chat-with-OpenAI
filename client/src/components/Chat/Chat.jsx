77df5115283956533dea1116908faa4277b42814
Gregory <maklakov.grigory@gmail.com>
2023-05-27 12:13:24 +0200

    add render chat <AiChat_  ...>

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -6,10 +6,11 @@ import {
 import CustomHeader from '../CustomHeader/CustomHeader';
 import StandardMessageForm from '../customMessageForm/StandardMessageForm/StandardMessageForm'
 import "./Chat.scss"
+import Ai from "../customMessageForm/Ai/Ai";
 
-// const projectId = import.meta.env.VITE_PROJECT_ID;
-const projectIdGinger = "7ad349b8-945b-4ed8-be7e-89b0a90704dc";
+const projectIdGinger = "7ad349b8-945b-4ed8-be7e-89b0a90704dc"
 
+// console.log("🚀 ~ file: Chat.jsx:13 ~ projectIdGinger:", projectIdGinger)
 
 const Chat = () => {
     const chatProps = useMultiChatLogic(
@@ -24,6 +25,11 @@ const Chat = () => {
             {...chatProps}
             renderChatHeader={(chat) => <CustomHeader chat={chat} />}
             renderMessageForm={(props) => {
+
+                if (chatProps.chat?.title.startsWith("AiChat_")) {
+                    return (<Ai props={props} activeChat={chatProps.chat} />)
+                }
+
                 return (
                     <StandardMessageForm props={props} activeChat={chatProps.chat} />
                 )
--- client/src/components/customMessageForm/Ai/Ai.jsx
+++ client/src/components/customMessageForm/Ai/Ai.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import MessageFormUI from '../MessageFormUI/MessageFormUI';
+import { usePostAiTextMutation } from '@/stare/api';
 
-const Ai = () => {
+const Ai = ({ props, activeChat }) => {
+
+    const [message, setMessage] = useState("");
+    const [attachment, setAttachment] = useState("");
+    const [trigger] = usePostAiTextMutation()
+
+    const handleChange = (e) => setMessage(e.target.value);
+
+    const handleSubmit = async () => {
+        const userTimezoneOffset = new Date().getTimezoneOffset() * 60000; // Получаем смещение времени пользователя в миллисекундах
+        const userDate = new Date(Date.now() - userTimezoneOffset); // Создаем новый объект Date с учетом смещения времени пользователя
+
+        const date = userDate
+            .toISOString()
+            .replace("T", " ")
+            .replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
+        const at = attachment ? [{ blob: attachment, file: attachment.name }] : [];
+        const form = {
+            attachments: at,
+            created: date,
+            sender_username: props.username,
+            text: message,
+            activeChatId: activeChat.id,
+        };
+
+        props.onSubmit(form);
+        trigger(form);
+        setMessage("");
+        setAttachment("");
+    };
+
+
     return (
-        <div>Ai</div>
-    )
+        <MessageFormUI
+            setAttachment={setAttachment}
+            message={message}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+        />
+    );
+
 }
 
 export default Ai
